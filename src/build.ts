20f6c6d49a02b6adc7363cc634272e34a5cf86d8
mcfitzgerald <michael.craig.fitzgerald@gmail.com>
2020-05-28 14:42:19 -0400

    first commit

---
+++ src/build.ts
@@ -0,0 +1,11 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+export async function build(): Promise<void> {
+  const path = core.getInput('path', {required: true})
+  try {
+    await exec.exec(`npx eleventy ${path}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {build} from './build'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    await build()
   } catch (error) {
     core.setFailed(error.message)
   }
