57c26544fab63c372b092d22153f54e6f0ef024e
mcfitzgerald <michael.craig.fitzgerald@gmail.com>
2020-05-28 19:52:49 -0400

    prefix @11ty

--- src/build.ts
+++ src/build.ts
@@ -4,7 +4,7 @@ import * as exec from '@actions/exec'
 export async function build(): Promise<void> {
   const path = core.getInput('path', {required: true})
   try {
-    await exec.exec(`npx eleventy ${path}`)
+    await exec.exec(`npx @11ty/eleventy ${path}`)
   } catch (error) {
     core.setFailed(error.message)
   }
